68e78dfdf3de4e77216f05ab3d12660ab640cfdd
Bemin <bemin_dawoud@yahoo.com>
2024-04-19 17:50:16 -0700

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,7 @@
+#!/usr/bin/pup
 # script config for install flask.
-
-package { 'python3-pip':
-  ensure => installed,
-}
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
