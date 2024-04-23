9042cac2d83e126d7ce32f33776a54b0515cd4fb
Bemin <bemin_dawoud@yahoo.com>
2024-04-23 05:13:23 -0700

    update flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,15 @@
 #!/usr/bin/pup
 # Install Flask version 2.1.0 using pip3
-package { 'python3':
+package { 'python3-pip':
   ensure => installed,
 }
-package { 'Werkzeug':
-  ensure   => '2.1.1',
-}
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+package { 'werkzeug':
+  ensure   => installed,
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
