d7abb5b9738b3e1d579fe714f7cf0e90e3bd2a5d
Bemin <bemin_dawoud@yahoo.com>
2024-04-30 10:13:16 -0700

    update lint

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,13 +9,11 @@ exec {'redirect_page':
 
 command => '/usr/bin/sudo /bin/sed -i "/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
 }
-exec {'404_page':
-
-command => '/usr/bin/sudo /usr/sbin/echo "Ceci n\'est pas une page" > /var/www/html/404.html',
-}
-exec {'add_header':
-
-command => '/usr/bin/sudo /usr/sbin/sudo sed -i "/listen 80 default_server/a add_header X-Served-By '$HOSTNAME';" /etc/nginx/sites-available/default',
+file_line { 'add_custom_header':
+  line  => 'add_header X-Served-By $HOSTNAME;',
+  path  => '/etc/nginx/sites-available/default',
+  match => '^listen 80 default_server',
+  notify  => Exec['restart_nginx'],
 }
 exec {'start_server':
 
