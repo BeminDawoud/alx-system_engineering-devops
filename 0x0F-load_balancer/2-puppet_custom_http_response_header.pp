bf3c2496ccd3e1dd4064fbe1b4b4a9af152c5835
Bemin <bemin_dawoud@yahoo.com>
2024-04-30 09:29:10 -0700

    create advanced

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,19 @@
+nstal nginx
+package { 'nginx':
+provider => 'apt',
+}
+exec {'hello-page':
+command => '/usr/bin/sudo /bin/echo "Hello World!" > /var/www/html/index.nginx-debian.html',
+}
+exec {'redirect_page':
+
+command => '/usr/bin/sudo /bin/sed -i "/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+}
+exec {'Add_Header':
+
+command => '/usr/bin/sudo /usr/sbin/sudo sed -i "/listen 80 default_server/a add_header X-Served-By '$HOSTNAME';" /etc/nginx/sites-available/default',
+}
+exec {'start_server':
+
+command => '/usr/bin/sudo /usr/sbin/service nginx start',
+}
