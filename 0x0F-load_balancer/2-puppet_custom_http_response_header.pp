2c0c6f5cc75b399a667b8913db0f13b793d92a36
Bemin <bemin_dawoud@yahoo.com>
2024-04-30 17:22:45 -0700

    Update puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,14 @@
 #install nginx
 package { 'nginx':
-provider => 'apt',
+ensure => present,
 }
-exec {'hello-page':
-command => '/usr/bin/sudo /bin/echo "Hello World!" > /var/www/html/index.nginx-debian.html',
-}
-exec {'redirect_page':
 
-command => '/usr/bin/sudo /bin/sed -i "/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-}
 file_line { 'add_custom_header':
   line  => 'add_header X-Served-By $HOSTNAME;',
   path  => '/etc/nginx/sites-available/default',
   match => '^listen 80 default_server',
-  notify  => Exec['restart_nginx'],
 }
-exec {'start_server':
+exec {'reload_server':
 
-command => '/usr/bin/sudo /usr/sbin/service nginx start',
+command => '/usr/bin/sudo /usr/sbin/service nginx restart',
 }
