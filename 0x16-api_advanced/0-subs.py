a8f74e3745b3916cb90426fe51ebbe1aab2afc00
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-06-04 23:17:08 +0300

    Update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,12 +3,11 @@
 returns the number of subscribers for a given subreddit.
 """
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit."""
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = f'https://www.reddit.com/r/{subreddit}/about/.json'
     headers = {'User-Agent': 'MyRedditApp/0.1 by Bemin'}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
