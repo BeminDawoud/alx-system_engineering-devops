448a89c103f221b0180e5c530573ff4f0be83a33
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-06-04 22:55:52 +0300

    Completed task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,8 +2,20 @@
 """
 returns the number of subscribers for a given subreddit.
 """
-from sys import argv
+import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    return 0
+    url = f'https://www.reddit.com/r/{subreddit}/.json'
+    headers = {'User-Agent': 'MyRedditApp/0.1 by YourUsername'}
+    try:
+        response = requests.get(url, headers=headers)
+        children = response.json().get('data').get('children')
+        subs = children[0].get("data").get("subreddit_subscribers")
+        if subs:
+            return children[0].get("data").get("subreddit_subscribers")
+        else:
+            return 0
+    except Exception:
+        return 0
