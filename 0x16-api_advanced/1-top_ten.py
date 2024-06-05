fe07eaac1075327e5dbb6e7ac45fc0f72d0511cc
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-06-05 19:21:50 +0300

    Update 1-top_ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-'''
-    prints the titles of the first 10 hot posts listed for a given subreddit.
-'''
+"""
+Prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
 import requests
 
 
 def top_ten(subreddit):
-    '''prints the titles of the first 10 posts for a given subreddit.'''
+    """Prints the titles of the first 10 hot posts for a given subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
     headers = {'User-Agent': 'MyRedditApp/0.1 by Bemin'}
-    response = requests.get(url, headers=headers).json()
     try:
-        for post in response.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            if 'application/json' in response.headers.get('Content-Type', ''):
+                data = response.json().get('data', {})
+                children = data.get('children', [])
+                for post in children:
+                    print(post.get('data', {}).get('title'))
+            else:
+                print(None)
+        else:
+            print(None)
+    except requests.RequestException as e:
         print(None)
